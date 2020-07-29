8f46f44af965992a48bd776efd4e2d299add1565
Jason Hur <jasonyhur@gmail.com>
2020-07-29 19:47:09 -0400

    initial commit

---
+++ src/pages/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  /*--- State ---*/
+
+
+  /*--- Handle Methods ---*/
+
+
+  /*--- Lifecycle Methods ---*/
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">MY APP</header>
+      </div>
+    );
+  }
+}
+export default App;
