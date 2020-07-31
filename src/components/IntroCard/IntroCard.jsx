e1e7052e6404c800ca15f5ea756586694e7cfe7f
Jason Hur <jasonyhur@gmail.com>
2020-07-31 14:05:51 -0400

    upload project links

---
+++ src/components/IntroCard/IntroCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './IntroCard.css'
+
+function IntroCard(){
+    return(
+        <div className='IntroCard'>
+
+
+
+          <div><img src="https://i.imgur.com/FqtriAq.png" alt="" className="Headshot" /></div>
+          <div className="IntroInfo"><h3>What I Do</h3><br></br><h5>I develop full stack web applications!</h5></div>
+
+
+        </div>
+    )
+}
+export default IntroCard;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Projects from '../../components/Projects/Projects';
+import IntroCard from '../../components/IntroCard/IntroCard';
 
 class App extends Component {
   
@@ -15,7 +17,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">MY APP</header>
+        <header className="App-header">
+          <h2>Hi, I'm Jason Hur</h2>
+        </header>
+        
+        <IntroCard />
+
+        <div className="Projects"><Projects /></div>
+
+        <footer> linkedin.com/in/jasonhur | github.com/jhur91 | jasonyhur@gmail.com | (805) 300 - 7818 <br></br></footer>
       </div>
     );
   }
